5618da7a41ed54ce54083240c46e5b8b8fc1dec2
gmoralis <giorgosmrls@gmail.com>
2013-05-02 13:10:20 +0300

    pspudb is now gpl2.0

--- pspudb/src/umdloader/isodrv.h
+++ pspudb/src/umdloader/isodrv.h
@@ -1,19 +1,19 @@
-/*
-This file is part of pcsp.
+// Copyright (c) 2013- PSPUDB Project.
 
-pcsp is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2.0 or later versions.
 
-pcsp is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License 2.0 for more details.
 
-You should have received a copy of the GNU General Public License
-along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
- */
+// A copy of the GPL 2.0 should have been included with the program.
+// If not, see http://www.gnu.org/licenses/
+
+// Official git repository and contact information can be found at
+// https://github.com/georgemoralis/pspudb and http://www.pspudb.com/
 #pragma once
 
 #include "isolib.h"
